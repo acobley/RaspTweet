da57456377bfa146a175f954440e9f408769fdaa
AndyCobley <acobley@computing.dundee.ac.uk>
2012-06-01 13:17:49 +0100

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaspTweet::Application.config.secret_token = 'aaa582b5591ada433bdce40a465bd8eaef4c7ba310837cd111aca70900da37294039782b4b537861a7a58966d8adbb2a0960a16780d4294f6c04a048221a52e2'
